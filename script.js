77d27b7ba09a6a5fc961e093e7e0098c8285320f
sievsitejs <matissilins6@gmail.com>
2025-02-14 10:23:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+alert("Hello World!")
+
+console.log("Hello World!");
+*/
+/*
+let x = 5;
+let y = 12;
+
+if(x == y)
+{
+console.log("x ir vienads ar y")
+}
+*/
+
+
+
+let number = prompt("enter number: ");
+if(number > 0)
+{
+    console.log("pozitīvs");
+}
+if (number <0)
+    {
+        console.log("negatīvs");
+    }
+    if(number ==0)
+    {
+        console.log(number, "ir 0");
+    }
